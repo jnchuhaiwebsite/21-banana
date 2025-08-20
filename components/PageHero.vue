ef6ebde92a71e017b3dcea5ff280d34f05aa4d28
zhangjianbin <2551490259@qq.com>
2025-08-20 09:06:48 +0800

    -

--- components/PageHero.vue
+++ components/PageHero.vue
@@ -3,7 +3,7 @@
     <h1 class="text-4xl font-extrabold tracking-tight sm:text-5xl lg:text-6xl bg-gradient-to-r from-banana-primary-yellow to-banana-secondary-blue text-transparent bg-clip-text py-2">
       <span class="block">{{ title }}</span>
     </h1>
-    <p v-if="subtitle" class="mx-auto mt-6 max-w-2xl text-lg tracking-tight text-banana-text-muted">
+    <p v-if="subtitle" class="mx-auto mt-6 max-w-5xl text-lg tracking-tight text-banana-text-muted">
       {{ subtitle }}
     </p>
   </div>
