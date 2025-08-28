f9da7a263c4f758785177b0648f81e299de87ec8
zhangjianbin <2551490259@qq.com>
2025-08-28 18:17:17 +0800

    -

--- components/banana/Generator.vue
+++ components/banana/Generator.vue
@@ -589,6 +589,13 @@ const applyPreset = (preset: Preset) => {
 };
 
 const editImage = async () => {
+  alert(await checkLoginStatus())
+  if (!await checkLoginStatus()) {
+    alert(1)
+    showUploadGuide.value = true;
+    return;
+  }
+
   if (selectedImages.value.length === 0) {
     $toast.error('Please upload an image first');
     return;
